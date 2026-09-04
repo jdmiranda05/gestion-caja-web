6131af372f90ccb4bc801772cece0830a094accd
Juan Miranda <jdmiranda0506@gmail.com>
2026-09-04 10:09:32 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/pe/edu/pe/gestion_caja_web/GestionCajaWebApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.gestion_caja_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionCajaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionCajaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/pe/gestion_caja_web/GestionCajaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pe.gestion_caja_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionCajaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
