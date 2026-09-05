43200a5c7e18dd5eaaf1b47bb9bd796894ae4d12
Sergio Rodriguez Espinoza <rodolfopiscisggt@gmail.com>
2026-09-04 23:04:12 -0500

    feat(sale): exponer endpoint GET /api/sales/{id}/receipt

--- src/main/java/pe/edu/pe/gestion_caja_web/sale/SaleController.java
+++ src/main/java/pe/edu/pe/gestion_caja_web/sale/SaleController.java
@@ -3,7 +3,9 @@ package pe.edu.pe.gestion_caja_web.sale;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,11 @@ public class SaleController {
     @ResponseStatus(HttpStatus.CREATED)
     public Sale register(@RequestBody SaleRequest request) {
         return saleService.register(request);
+    }
+
+    @GetMapping("/{id}/receipt")
+    public ResponseEntity<ReceiptResponse> getReceipt(@PathVariable Long id) {
+        ReceiptResponse receipt = saleService.getReceiptBySaleId(id);
+        return ResponseEntity.ok(receipt);
     }
 }
